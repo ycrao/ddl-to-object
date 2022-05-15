bcc40ea557146284b29b43731962c31a31401ed8
raoyc <raoyc@foxmail.com>
2022-05-15 20:47:34 +0800

    add output

--- cmd/go.go
+++ cmd/go.go
@@ -1,7 +1,10 @@
 package cmd
 
-
-import "github.com/spf13/cobra"
+import (
+	"ddl-to-object/lib"
+	"github.com/spf13/cobra"
+	"strings"
+)
 
 func init() {
 	rootCmd.AddCommand(goCmd)
@@ -10,4 +13,30 @@ func init() {
 var goCmd = &cobra.Command{
 	Use: "go",
 	Short: "generate golang target object file",
+	Run: func(cmd *cobra.Command, args []string) {
+		// set default package name to `main`
+		goPackage := "main"
+		if pk != "" {
+			packageArr := strings.Split(pk, ".")
+			if len := len(packageArr); len > 0 {
+				goPackage = packageArr[len - 1]
+			}
+		}
+		toDir := "./"
+		if to != "" {
+			err := lib.VisitLocationInWriteMode(to)
+			if err != nil {
+				panic(err)
+			}
+			toDir = to
+		}
+		if from != "" {
+			content, err := lib.ReadFile(from)
+			if err != nil {
+				panic(err)
+			}
+			lib.Parse(string(content))
+		}
+
+	},
 }
